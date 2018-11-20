20a8b021e5d4829a0698ada6418d2f062cb24a7b
eddie-liang <44708858+eddie-liang@users.noreply.github.com>
2018-11-20 09:45:46 -0500

    Add files via upload

---
+++ flask_opencv_realtime_api/camera_opencv.py
@@ -0,0 +1,30 @@
+import cv2
+from base_camera import BaseCamera
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+class Camera(BaseCamera):
+    video_source = 0
+
+    @staticmethod
+    def set_video_source(source):
+        Camera.video_source = source
+
+    @staticmethod
+    def frames():
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        while True:
+            # read current frame
+            _, img = camera.read()
+            faces = face_cascade.detectMultiScale(img, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            # encode as a jpeg image and return it
+            yield cv2.imencode('.jpg', img)[1].tobytes()
